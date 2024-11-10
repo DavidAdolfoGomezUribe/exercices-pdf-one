04971f9680d8b6f9a821aefb8f8a627a765417ea
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-10 14:08:11 -0500

    Solution to exercise 16

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Ejercicio 15: Cálculo del salario neto 
-#Escribe un programa que calcule el salario neto de un empleado después de aplicar impuestos.
+#Ejercicio 16: Cálculo del tiempo de viaje 
+#Escribe un programa que calcule el tiempo que tarda en llegar un automóvil a su destino.
 # Enunciado: 
-#Solicita al usuario su salario bruto y su país de residencia. Calcula el salario neto aplicando 
-#impuestos: el 20% para residentes de "País A", el 15% para "País B" y el 10% para "País C". Si el 
-#país no está en la lista, aplica un 25% de impuestos
+#Solicita al usuario la distancia a recorrer (en km) y la velocidad promedio del automóvil (en km/h). 
+#Calcula el tiempo de viaje en horas y minutos. Si la velocidad es mayor a 120 km/h, muestra un 
+#mensaje de advertencia
 
 import sys
 import vlc
@@ -19,44 +19,25 @@ p = vlc.MediaPlayer("https://github.com/DavidAdolfoGomezUribe/clases-main/raw/re
 
 while True:
     try:
-        print(f"""\nHello Mrs/Ms {name} this is a program for calculate the tax rate on your country\n""")
-        
+        print(f"""\nHello Mrs/Ms {name} this is a program for calculate the travel time for a car at certain speed\n""")
 
-        sCountry = (input(f"""        Selec your country:
-        1) For country A 
-        2) For country B
-        3) For country C
-        (Press enter if your country its not in this list, the tax rate will be 25% )
-        """))
+        km = float(input("        Enter the distant (Km): "))
+        carSpeed = float(input("        Enter the Speed (km/H): "))
+
+        travelTime = km / carSpeed
+
+        hours = (int(travelTime))
+        minutes = (travelTime - hours) * 60
+        if travelTime > 0:
+            if carSpeed > 120: 
+                print("        CAUTION HIGH SPEED")
+
+            print(f"        The time travel is {hours} Hours and {round(minutes)} minuts ")
+        else: 
+            print("        No negative numbers permitted")
 
         
-        if sCountry == "1":
-            tax = 0.20
-            salary = abs(float(input("        Insert your salary: ")))
-            fSalary = salary - (salary * tax ) 
-            print(f"\n        For your salary of {salary}$ and a tax rate of {tax*100}%. Your real inconme after tax is: {fSalary}$")
         
-        elif sCountry == "2":
-            tax = 0.15
-            salary = abs(float(input("        Insert your salary: ")))
-            fSalary = salary - (salary * tax ) 
-            print(f"\n        For your salary of {salary}$ and a tax rate of {tax*100}%. Your real inconme after tax  is: {fSalary}$")
-        
-        elif sCountry == "3":
-            tax = 0.10
-            salary = abs(float(input("        Insert your salary: ")))
-            fSalary = salary - (salary * tax ) 
-            print(f"\n        For your salary of {salary}$ and a tax rate of {tax*100}%. Your real inconme after tax  is: {fSalary}$")
-    
-        elif sCountry == "":
-            tax = 0.25
-            salary = abs(float(input("        Insert your salary: ")))
-            fSalary = salary - (salary * tax ) 
-            print(f"\n        For your salary of {salary}$ and a tax rate of {tax*100}%. Your real inconme after tax  is: {fSalary} $")
-        else:
-            print ("        Enter a correct input")
-    
-
         continueToAsk = input("    Do you want to calculate again? : ").strip().lower()
         
         if continueToAsk == "yes" :
@@ -94,7 +75,7 @@ while True:
             for line in lines:
                 print(line)
                 time.sleep(0.41)
-            print("    Thanks for using the program")
+            print("""    Thanks for using the program""")
 
             break
     
@@ -143,4 +124,4 @@ while True:
 
 
 
-#Last line of code 
+ #Last line of code 
