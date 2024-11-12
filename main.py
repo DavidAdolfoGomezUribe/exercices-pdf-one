ee03e7fc9248a401e8d43ce087c95c6906e2fc99
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-11 21:32:13 -0500

    Solution to exercise 27

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# Ejercicio 26: Adivina el número (con 
-#while
-# ) 
-#Enunciado: (random.randint(1, 100))
-# Escribe un programa que genere un número aleatorio entre 1 y 100 y permita al usuario 
-#adivinarlo. El programa debe dar pistas si el número ingresado es mayor o menor que el número 
-#secreto. Usa un ciclo 
-#while para permitir al usuario seguir intentando hasta que adivine el 
-#número
+#Ejercicio 7: Suma de números pares hasta que se introduce 
+#un impar
+# Enunciado:
+# Escribe un programa que solicite al usuario ingresar números enteros indefinidamente. El 
+#programa debe sumar los números siempre que sean pares, pero debe detenerse si el usuario 
+#ingresa un número impar. Usa un ciclo 
+#while para lograr esto
 
 
 
@@ -22,23 +20,22 @@ name = input("Hello,please enter your name:")
 p = vlc.MediaPlayer("https://github.com/DavidAdolfoGomezUribe/clases-main/raw/refs/heads/main/music/chad.mp3")    
 
 while True:
+    print(f"""\nHello Mrs/Ms {name} this is a program for calculate the sum of even numbers \n""")
     try:
-        rand = random.randint(1,100)         
-        print(f"""\nHello Mrs/Ms {name} this is a program for adivinate a number based on the numbers  (1 to 100) \n""")
-        awnser = int(input("        Put the number: "))
-        
+        num=0
+        a = 0
+        while True:
+            try:    
+                num = int(input("        Enter the number: "))
 
-        while True : 
-            if awnser < rand:
-                print("\n        the number is lower")
-                awnser = int(input("        Put other number: "))
-            elif awnser > rand:        
-                print("\n        the number is higher")
-                awnser = int(input("        Put other number: "))
-            elif awnser == rand:
-                print(f"        Excellent you have discovered the number that is {awnser}\n")
-                break
-
+                if not (num % 2 == 0 ):
+                    break
+                else:
+                    a =+ num + a 
+                    print(f"        The actual sum is {a}\n")
+            except:
+                print("\n        Please enter a valid character\n")
+                
 
 
         continueToAsk = input("    Do you want to play again?(YES/NO) : ").strip().lower()
